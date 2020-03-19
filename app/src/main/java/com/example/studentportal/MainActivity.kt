b888c17266a36a3815c7a50bfe21f4149138b1de
akubishon <info.enesbaran@gmail.com>
2020-03-19 03:22:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/studentportal/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.studentportal
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+const val ADD_PORTAL_REQUEST_CODE = 100
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        initViews()
+        //floating action button
+        fab.setOnClickListener {
+            startAddActivity()
+        }
+    }
+
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals) { portalItem: Portal -> portalItemClicked(portalItem)}
+
+    private fun startAddActivity() {
+        val intent = Intent(this, AddActivity::class.java)
+        startActivityForResult(intent, ADD_PORTAL_REQUEST_CODE)
+    }
+
+
+    private fun initViews() {
+        rvPortals.layoutManager = GridLayoutManager(this@MainActivity, 2)
+        rvPortals.adapter = portalAdapter
+    }
+
+    private fun portalItemClicked(portalItem: Portal) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        customTabsIntent.launchUrl(this, Uri.parse(portalItem.url))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when(requestCode) {
+                ADD_PORTAL_REQUEST_CODE -> {
+                    val portal = data!!.getParcelableExtra<Portal>(EXTRA_PORTAL)
+                    portals.add(portal)
+                    portalAdapter.notifyDataSetChanged()
+                }
+                else -> {
+                    super.onActivityResult(requestCode, resultCode, data)
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
